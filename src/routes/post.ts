8bf832af96df020a3ad484b3388e3fece3115306
kimheeseok <kim1140129@gmail.com>
2023-01-05 14:07:48 +0900

    FIX: response 추가

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -7,6 +7,8 @@ router.post('/',async (req:Request, res:Response) => {
     try {
         const {title,content} = req.body;
         await Posts.create({title, content});
+
+        res.json({message: "등록되었습니다."})
     }catch (error){
         console.log(error);
     }
